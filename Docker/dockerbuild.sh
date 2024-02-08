eb206b917d089b3ebcafd307a838986c040a72dc
tgoelles <thomas.goelles@gmail.com>
2024-02-08 13:52:40 +0100

    better build

--- Docker/dockerbuild.sh
+++ Docker/dockerbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-docker build --rm -f ".devcontainer/Dockerfile" -t ghcr.io/tgoelles/modellieren:latest "."
+docker build --rm -f "Dockerfile" -t ghcr.io/tgoelles/modellieren:latest "."
 docker push ghcr.io/tgoelles/modellieren:latest    ✔ │ 1m 45s  │ base  │ 13:41:23 
 
